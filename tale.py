7efc086bb4b68db94899783b26be0d67f8f04806
王爵nice <biezhi.me@gmail.com>
2017-03-04 15:35:23 +0800

    Update tale.py

--- tale.py
+++ tale.py
@@ -7,6 +7,16 @@ stop_operation  = ['shutdown', 'stop']
 status_operation  = ['status']
 show_operation  = ['show', 'log']
 restart_operation  = ['reload']
+env = '-Xms128m -Xmx128m'
+
+# search tale-xxx.jar
+def search(path, word):
+    for filename in os.listdir(path):
+        fp = os.path.join(path, filename)
+        if os.path.isfile(fp) and word in filename:
+            return fp
+        elif os.path.isdir(fp):
+            search(fp, word)
 
 # get tale pid
 def get_pid():
@@ -19,12 +29,14 @@ def exist():
     return pid != ''
 
 # startup tale
-def startup():
+def startup(jar_name):
     if exist():
         print 'Tale already startup!'
     else:
         # start tale
-        os.system('java -jar ../tale.jar > ./tale.out < /dev/null &')
+        cmd = 'java '+ env +' -jar '+ jar_name +' > ./tale.out < /dev/null &'
+        print 'cmd: ' + cmd
+        os.system(cmd)
         show_log()
     pass
 
@@ -40,7 +52,11 @@ def show_log():
     pass
 
 def show_help():
-    print 'Usage: tale start|stop|reload|status|log]'
+    print '\r\n\t欢迎使用Tale Blog :)'
+    print '\r\nUsage: tale start|stop|reload|status|log'
+    print '\r\n可选参数: [-env] [-h]'
+    print '\t-env\t\tjvm相关参数'
+    print '\t-h\t\t帮助信息'
 
 def show_status():
     if exist():
@@ -53,16 +69,25 @@ def main():
     operation = ''
     sleeptime = 0
     arglen = len(sys.argv)
-    if arglen == 3:
-    	sleeptime = sys.argv[2]
+    
+    if arglen >= 3:
+        if isinstance(sys.argv[2], int):
+            sleeptime = sys.argv[2]
+    	if sys.argv[2] == '-env':
+            global env 
+            env = sys.argv[3]
+        if sys.argv[1] == '-h':
+            show_help()
+            sys.exit(0)   
     try:
 	    operation = sys.argv[1]
     except:
         show_help()
         sys.exit(0)
-        
+    
+    jar_name = search(os.path.pardir,'.jar')
     if operation in start_operation:
-        startup()
+        startup(jar_name)
     elif operation in stop_operation:
         if exist() == False:
             print 'Tale already shutdown!'
@@ -73,7 +98,7 @@ def main():
     elif operation in restart_operation:
         shutdown()
         time.sleep(float(sleeptime))
-        startup()
+        startup(jar_name)
     elif operation in status_operation:
         show_status()
     elif operation in show_operation:
